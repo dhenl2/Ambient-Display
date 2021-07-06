56547b308e22d6437c6546d5922094ab158eadc7
Daniel Henley <69876000+dhenl2@users.noreply.github.com>
2021-07-06 10:32:42 +1000

    Added lots of additional functionality
    Changed to using milliseconds for increased accuracy. Removed variance as standard deviation was found to be more useful. Added a name for each sensor for recognition. Added more flexibility and identifiability for occasions where a "person" has been found. Added timeout for events where a person has been found to remove false readings.

--- door_sensor_src/main.py
+++ door_sensor_src/main.py
@@ -1,70 +1,99 @@
 from hcsr04 import HCSR04
-import time
+import utime
 import math
 
+STD_THRESHOLD = 1  # if accuracy has a spread larger than 1 std then retry
+FOUND_TIMEOUT = 2  # (s)
 
-def get_variance(trials):
+def get_std(trials):
     count = len(trials)
     mean = sum(trials) / count
     variance = 0
     for x in trials:
         variance += abs(x - mean) / count
-    print("Variance: " + str(variance))
     std = math.sqrt(variance)
-    print("Standard Deviation: " + str(std))
-    return variance, std
-
+    return std
 
 class Sensor:
-    def __init__(self, sensor):
+    def __init__(self, sensor, name):
+        self.name = name
         self.sensor = sensor
-        self.calibrated = 0
-        self.variance = 0
+        self.average = 0
         self.std = 0
-        self.found = None
+        self.found = False
+        self.found_time = None
+        # self.calibration_attempt = 0
+
+    def reset_found(self):
+        self.found = False
+        self.found_time = None
+
+    def get_found_dt(self):
+        curr_time = utime.ticks_ms()
+        return (curr_time - self.found_time) / 1000
 
     def calibrate(self):
+        print("Calibrating " + self.name)
         trials = list()
-        time_start = time.time()
+        time_start = utime.ticks_ms()
         for i in range(1000):
             trials.append(self.sensor.distance_cm())
-        time_end = time.time()
-        print("Took " + str(time_end - time_start) + "s for 1000 trials")
-        self.calibrated = int(sum(trials) / len(trials))
-        self.variance, self.std = get_variance(trials)
-        print("Calibrated: " + str(self.calibrated))
+        time_end = utime.ticks_ms()
+        print("Took " + str((time_end - time_start)/1000) + "s for 1000 trials")
+        self.average = int(sum(trials) / len(trials))
+        self.std = get_std(trials)
+        print("Averaged: " + str(self.average) + " Std: " + str(self.std))
+        # if self.calibration_attempt < 5 and self.std > STD_THRESHOLD:
+        #     print("Attempt to recalibrate")
+        #     self.calibration_attempt += 1
+        #     self.calibrate()
 
     def someone_found(self, distance):
         # if distance < (self.calibrated - self.std):
-        if distance < (self.calibrated - 10) and distance != 0:
-            print("someone found with " + str(distance) + " < (" + str(self.calibrated) + " - 10)")
-            self.found = time.time()
+        if distance < (self.average - 10) and distance != 0:
+            print("someone found with " + str(distance) + " < (" + str(self.average) + " - 10)")
+            self.found = True
+            self.found_time = utime.ticks_ms()
 
-    def get_distance(self):
+    def get_distance(self, raw=False):
         distance = self.sensor.distance_cm()
-        if (distance > (self.calibrated + self.std)) or (distance == 0):
-            # ignore invalid result
-            #   - larger than 68% of results above the mean which is most likely an error
-            #   - or just 0 which is defs an error
-            return None
-        return self.sensor.distance_cm()
+        if not raw:
+            if (distance > (self.average + (self.std * 2))) or (distance == 0):
+                print("invalid sensor data for " + self.name)
+                print(str(distance) + " > (" + str(self.average) + " + " + str(self.std * 2) + ") or " + str(
+                    distance) + " == 0")
+                # ignore invalid result
+                #   - larger than 68% of results above the mean which is most likely an error
+                #   - or just 0 which is defs an error
+                return None
+        return distance
 
-
-def write_data(file, left_sensor, right_sensor):
-    left_distance = left_sensor.get_distance()
-    right_distance = right_sensor.get_distance()
-
+def check_for_passers(left_sensor, right_sensor):
     # check if someone has passed
-    if left_sensor.found is not None and right_sensor.found is not None:
+    if left_sensor.found and right_sensor.found:
         # check which direction they've come
-        if left_sensor.found < right_sensor.found:
+        if left_sensor.found_time < right_sensor.found_time:
             print("Someone came in")
-        elif right_sensor.found > left_sensor.found:
+        elif right_sensor.found_time > left_sensor.found_time:
             print("Someone left")
         else:
             print("We've got a wild one boys")
-        left_sensor.found = None
-        right_sensor.found = None
+            print(str(right_sensor.found_time) + " ? " + str(left_sensor.found_time))
+        left_sensor.reset_found()
+        right_sensor.reset_found()
+    else:
+        # check if false reading given or someone attempted to pass in but did not go all the way in
+        if left_sensor.get_found_dt() > FOUND_TIMEOUT:
+            left_sensor.reset_found()
+        if right_sensor.get_found_dt() > FOUND_TIMEOUT:
+            right_sensor.reset_found()
+
+
+def write_data(file, left_sensor, right_sensor, start_time, header=False):
+    left_distance = left_sensor.get_distance(True)
+    right_distance = right_sensor.get_distance(True)
+
+    check_for_passers(left_sensor, right_sensor)
 
     # Check if distance is invalid or check if someone is passing
     if left_distance is None:
@@ -79,34 +108,32 @@ def write_data(file, left_sensor, right_sensor):
         right_distance = int(right_distance)
         right_sensor.someone_found(right_distance)
 
-    data = str(left_sensor.calibrated) + "," + str(left_distance) + "," + \
-           str(right_sensor.calibrated) + "," + str(right_distance) + "\n"
+    time_diff = utime.ticks_ms() - start_time
+    data = str(time_diff) + "," + str(left_sensor.average) + "," + str(left_distance) + "," + \
+           str(right_sensor.average) + "," + str(right_distance) + "\n"
+    if header:
+        file.write("time (s),Left (avg),Left,Right (avg),Right\n")
     file.write(data)
 
-
 def main():
-    sensor_right = Sensor(HCSR04(trigger_pin=5, echo_pin=4))
-    sensor_left = Sensor(HCSR04(trigger_pin=14, echo_pin=12))
-    print("Left")
+    sensor_left = Sensor(HCSR04(trigger_pin=14, echo_pin=12), "left")
+    sensor_right = Sensor(HCSR04(trigger_pin=5, echo_pin=4), "right")
     sensor_left.calibrate()
-    print("Right")
     sensor_right.calibrate()
-    # while True:
-    #     print("Left: " + str(int(sensor_left.get_distance())) + "cm\nRight: " + str(int(sensor_right.get_distance())) + "cm")
-    #     time.sleep(0.4)
 
     # record distance
     file = open("record.csv", "w")
     print("Starting Recording Session")
-    file.write("Left,Left (avg),Right,Right (avg)\n")
-
     count = 0
-    while count < 500:
-        write_data(file, sensor_left, sensor_right)
+    start_time = utime.ticks_ms()
+    while count < 1000:
+        if count == 0:
+            write_data(file, sensor_left, sensor_right, start_time, True)
+        else:
+            write_data(file, sensor_left, sensor_right, start_time)
         count += 1
     file.close()
     print("Ending Recording Session")
-
 
 if __name__ == "__main__":
     main()
