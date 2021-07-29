750ebc7ec191f6c063ba5ba20a10cac24217482b
Daniel Henley <69876000+dhenl2@users.noreply.github.com>
2021-07-29 23:17:51 +1000

    Stopped threaded program from returning from main

--- Server_src/main.py
+++ Server_src/main.py
@@ -2,7 +2,7 @@ from _thread import *
 from Server import start_server, client_thread, ServerInfo
 from threading import Lock
 
-def run_server(clients):
+def run_server():
     sock = start_server("", 8123)
     lock = Lock()
     server = ServerInfo(lock)
@@ -10,21 +10,14 @@ def run_server(clients):
         print(f"Waiting for a connection...")
         con, addr = sock.accept()
         print(f"Connected to {addr}")
-        start_new_thread(client_thread, (con, clients, server, ))
-        # command = input("Input command: ")
-        # command += '\n'
-        # con.send(bytes(command, 'utf-8'))
-        # result = con.recv(1024)
-        # print("Received : " + str(result.decode("utf-8")))
-
+        start_new_thread(client_thread, (con, server, ))
 
 def main():
-    # clients = microphone, door_sensor, display
-    clients = list(), list(), list()
-    start_new_thread(run_server, (clients, ))
+    start_new_thread(run_server, ())
+    while True:
+        pass
     # TODO thread to determine reading to be sent to display based on input
-
-
+    # TODO add signal handler for SIGKILL/SIGINT to close all files
 
 
 if __name__ == "__main__":
