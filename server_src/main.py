928da5beef6697fba518ca4764f18bac6f97d73d
Daniel Henley <69876000+dhenl2@users.noreply.github.com>
2021-09-13 16:37:06 +1000

    Update main.py
    Changed relative directory to be explicit. Added control for calibration periods to only occur outside of the sheds operating times.

--- server_src/main.py
+++ server_src/main.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
 import sys
 from Server import start_server, ServerInfo
 from Client import client_thread
 from threading import Lock, Thread
 import signal
 import time
+import os
+
+directory = "/home/pi/Python/Log/"
 
 def run_server(server):
     sock = start_server("0.0.0.0", 8123)
@@ -17,15 +21,13 @@ def run_server(server):
         server.threads.append(thread_id)
 
 def main():
-
-
     def stop_server(signal_no, frame):
         print("Received signal " + str(signal_no))
         server.quit_clients()
         for thread in server.threads:
             # got 5s to come back properly or your getting a smack
             thread.join(5)
-        server.activity_level.log_file.close()
+        server.activity_level.close_log_files()
         sys.exit()
 
     def time_to_calibrate(signal_no, frame):
@@ -34,19 +36,48 @@ def main():
         server.calibrate_now = True
 
     def routine_calibration():
+        shed_open = 7
         shed_close = 14
-        while True:
+        open_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+        def shed_closed():
             try:
                 current_hour = int(time.strftime("%H"))
+                today = time.strftime("%A")
             except ValueError:
-                continue
-            if (current_hour > shed_close and server.activity_level.calibrate_again) or server.calibrate_now:
-                # pause all clients from adding new data
-                print("It's now past the shed close time. Time to calibrate")
-                server.set_clients_pause(True)
-                server.activity_level.create_activity_levels()
-                server.set_clients_pause(False)
-            time.sleep(60 * 60 * 3)     # run this every 3hrs
+                return False
+            return current_hour > shed_close or current_hour < shed_open
+
+        def shed_is_open():
+            try:
+                current_hour = int(time.strftime("%H"))
+                today = time.strftime("%A")
+            except ValueError:
+                return False
+            return today in open_days and (shed_open <= current_hour <= shed_close)
+
+        def log_files_available():
+            return len(os.listdir(directory)) > 2
+
+        start_timer = time.time()
+        leave_on = False
+        while True:
+            if shed_closed() or not leave_on:
+                print(f"closing shed")
+                server.set_display_clients(False)
+                if server.activity_level.calibrate_again and log_files_available() or server.calibrate_now:
+                    # pause all clients from adding new data
+                    print("It's now past the shed close time. Time to calibrate")
+                    server.set_clients_pause(True)
+                    server.activity_level.create_activity_levels()
+                    server.set_clients_pause(False)
+            # elif shed_is_open():
+            # print(f"Current timer: {time.time() - start_timer}")
+            # if time.time() - start_timer > 30:
+            #     print("opening shed")
+            #     server.set_display_clients(True)
+            #     leave_on = True
+            time.sleep(60 * 60 * 2)     # run this every 3hrs
 
     signal.signal(signal.SIGINT, stop_server)
     signal.signal(signal.SIGQUIT, stop_server)
