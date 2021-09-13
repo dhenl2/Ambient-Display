80ecb0e0b11dab053797f620cdd9f42d3665634f
Daniel Henley <69876000+dhenl2@users.noreply.github.com>
2021-09-13 16:34:40 +1000

    Update ActivityLevel.py
    Added log files for calibration levels and current activity levels. Separated how calibration is done. Now calibrates based on the data from either microphone or door sensors. Simplified the time stamp of the log. Change location of log files to be from /home rather than a relative directory.  Added error checking for invalid files.

--- server_src/ActivityLevel.py
+++ server_src/ActivityLevel.py
@@ -3,6 +3,8 @@ import numpy as np
 import time
 import math
 
+directory = "/home/pi/Python/Log/"
+
 class FixedQueue:
     def __init__(self, max_size):
         self.queue = []
@@ -24,22 +26,28 @@ class ActivityLevel:
         self.mic_levels = {}
         self.mic_mean = 59.81290416
         self.mic_std = 3.938022299
-        self.log_file = open("Log/activity_level.csv" , "a")
-        if len(os.listdir("Log/")) > 1:
+        self.calibration_file = open(directory + "activity_calibration.csv", "a")
+        self.activity_log_file = open(directory + "activity_levels.csv", "a")
+        self.calibrate_again = True
+        print(f"Got {len(os.listdir(directory))} log files: {len(os.listdir(directory)) > 2}")
+        if len(os.listdir(directory)) > 2:
             self.create_activity_levels()
         else:
             self.set_default_levels()
-        self.calibrate_again = True
         # print("Finished constructing ActivityLevels")
 
-    def set_default_levels(self):
+    def close_log_files(self):
+        self.calibration_file.close()
+        self.activity_log_file.close()
+
+    def set_default_levels(self, ):
         print("Setting default levels")
         set_levels(self.mic_levels, self.mic_mean, self.mic_std)
         set_levels(self.people_levels, self.people_mean, self.people_std)
-        self.log_activity_levels(self.mic_levels, "Microphone", self.mic_mean, self.mic_std)
-        self.log_activity_levels(self.people_levels, "People", self.people_mean, self.people_std)
-    
-    def log_activity_levels(self, levels, level_name, mean, std):
+        self.log_calibration_levels(self.mic_levels, "Microphone", self.mic_mean, self.mic_std)
+        self.log_calibration_levels(self.people_levels, "People", self.people_mean, self.people_std)
+
+    def log_calibration_levels(self, levels, level_name, mean, std):
         if not time_to_log():
             return
         print("Logging activity levels")
@@ -52,7 +60,7 @@ class ActivityLevel:
             bounds.replace(", ", "-")
             record += bounds
         record += "\n"
-        self.log_file.write(record)
+        self.calibration_file.write(record)
 
     def create_activity_levels(self):
         print("Creating activity levels")
@@ -61,36 +69,55 @@ class ActivityLevel:
         mic_files, door_files = get_data_files()
         mic_data = get_data_from_files(mic_files)
         door_data = get_data_from_files(door_files)
-        print(f"got {len(mic_files)} mic files with {len(mic_data)} data points")
-        print(f"got {len(door_files)} door files with {len(door_data)} data points")
-        # record prev mean and std and save current mean and std
-        prev_mic = self.mic_mean, self.mic_std
-        prev_people = self.people_mean, self.people_std
-        self.mic_mean = mic_data.mean()
-        self.mic_std = mic_data.std()
-        self.people_mean = door_data.mean()
-        self.people_std = door_data.std()
-        print(f"Mic mean and std {(self.mic_mean, self.mic_std)}")
-        print(f"Door mean and std {(self.people_mean, self.people_std)}")
-        # determine levels
-        set_levels(self.mic_levels, self.mic_mean, self.mic_std)
-        set_levels(self.people_levels, self.people_mean, self.people_std)
-        self.log_activity_levels(self.mic_levels, "Microphone", self.mic_mean, self.mic_std)
-        self.log_activity_levels(self.people_levels, "People", self.people_mean, self.people_std)
-        # check if calibration is the same as before
-        if (within_bounds(self.mic_mean, prev_mic[0], 4) and within_bounds(self.mic_std, prev_mic[1], 4)) or \
-                (within_bounds(self.people_mean, prev_people[0], 3) and within_bounds(self.people_std, prev_people[1], 3)):
+        calibrate_mic = calibrate_people = True
+        if len(mic_data) == 0:
+            print("Setting default for mic levels")
+            set_levels(self.mic_levels, self.mic_mean, self.mic_std)
+        else:
+            print(f"got {len(mic_files)} mic files with {len(mic_data)} data points")
+            prev_mic = self.mic_mean, self.mic_std
+            self.mic_mean = mic_data.mean()
+            self.mic_std = mic_data.std()
+            print(f"Mic mean and std {(self.mic_mean, self.mic_std)}")
+            set_levels(self.mic_levels, self.mic_mean, self.mic_std)
+            self.log_calibration_levels(self.mic_levels, "Microphone", self.mic_mean, self.mic_std)
+            # if within_bounds(self.mic_mean, prev_mic[0], 4) and within_bounds(self.mic_std, prev_mic[1], 4):
+            #     calibrate_mic = False
+
+        if len(door_data) == 0:
+            print("Setting default for door levels")
+            set_levels(self.people_levels, self.people_mean, self.people_std)
+        else:
+            print(f"got {len(door_files)} door files with {len(door_data)} data points")
+            prev_people = self.people_mean, self.people_std
+            self.people_mean = door_data.mean()
+            self.people_std = door_data.std()
+            print(f"Door mean and std {(self.people_mean, self.people_std)}")
+            # determine levels
+            set_levels(self.people_levels, self.people_mean, self.people_std)
+            self.log_calibration_levels(self.people_levels, "People", self.people_mean, self.people_std)
+            # if within_bounds(self.people_mean, prev_people[0], 3) and within_bounds(self.people_std, prev_people[1], 3):
+            #     calibrate_people = False
+
+        if not calibrate_mic and not calibrate_people:
             self.calibrate_again = False
         time_taken = time.time() - start_time
         print(f"Taken {time_taken}s or {time_taken/60}min or {time_taken/ (60 * 60)}hrs")
 
+    def log_level(self, level):
+        time_stamp = time.strftime("%d-%m-%Y-%H:%M:%S")
+        record = f"{time_stamp},{level}\n"
+        self.activity_log_file.write(record)
+
+
     def get_level(self):
         people_lvl = get_level_of(self.people.get_count(), self.people_levels)
-        print(f"Got people level {people_lvl}")
+        print(f"Got people level {people_lvl} with {self.people.get_count()}")
         mic_lvl = get_level_of(self.get_microphone_avg(), self.mic_levels)
-        print(f"Got mic level {mic_lvl}")
+        print(f"Got mic level {mic_lvl} with {self.get_microphone_avg()}")
         level = math.floor((people_lvl + mic_lvl) / 2)
         print(f"Got level: {level}")
+        self.log_level(level)
         return level
 
     def get_microphone_avg(self):
@@ -187,7 +214,7 @@ def get_level_of(value, level_map):
 def get_data_from_files(files):
     data = []
     for data_file in files:
-        file = open("Log/" + data_file, "r")
+        file = open(directory + data_file, "r")
         line = file.readline()
         while line != "":
             # process line
@@ -195,22 +222,30 @@ def get_data_from_files(files):
             if len(line_split) > 1:
                 value = line_split[1]
                 data.append(float(value))
+                line = file.readline()
             else:
                 # invalid data found
-                continue
+                line = file.readline()
     return np.array(data)
 
 def get_data_files():
-    files = os.listdir("Log/")
+    files = os.listdir(directory)
     mic_files = []
     door_files = []
     for file in files:
         if not file == "activity_level.csv":
             file_split = file.split("-")
-            if file_split[3] == "microphone.csv":
-                mic_files.append(file)
-            elif file_split[3] == "door_sensor.csv":
-                door_files.append(file)
+            if len(file_split) == 1:
+                continue
+            try:
+                if file_split[3] == "microphone.csv":
+                    mic_files.append(file)
+                elif file_split[3] == "door_sensor.csv":
+                    door_files.append(file)
+            except IndexError as e:
+                print("Got IndexError: " + str(e))
+                mic_files = door_files = []
+                break
     return mic_files, door_files
 
 def time_to_log():
