7a8fb7d53afac08f4637e332321925dd173309c0
Daniel Henley <69876000+dhenl2@users.noreply.github.com>
2021-10-10 21:02:30 +1000

    Modified gps library to get datestamp and timestamp. Added py file to get local time

---
+++ Server_src/gps_local_time.py
@@ -0,0 +1,27 @@
+from pa1010d import PA1010D
+import time
+import datetime
+import pytz
+
+gps = PA1010D()
+
+
+def get_local_time(timestamp, datestamp):
+    timezone = pytz.timezone("Australia/Brisbane")
+    utc_time = datetime.datetime.combine(datestamp, timestamp)
+    utc_time = pytz.UTC.localize(utc_time)
+    current_time = utc_time.astimezone(timezone)
+    current_time = current_time.replace(tzinfo=None)
+    return current_time.isoformat(sep=" ")
+
+
+def main():
+    while True:
+        time.sleep(1)
+        gps.update()
+        if gps.timestamp is not None and gps.datestamp is not None:
+            print(get_local_time(gps.timestamp, gps.datestamp))
+
+
+if __name__ == "__main__":
+    main()
